871a3a74603fc7a211081db4a1b0a9085313b351
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2024-05-11 09:56:38 -0300

    Payment, one-to-one association

--- src/main/java/com/projetodemo/course/config/TestConfig.java
+++ src/main/java/com/projetodemo/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetodemo.course.entities.Category;
 import com.projetodemo.course.entities.Order;
 import com.projetodemo.course.entities.OrderItem;
+import com.projetodemo.course.entities.Payment;
 import com.projetodemo.course.entities.Product;
 import com.projetodemo.course.entities.User;
 import com.projetodemo.course.entities.enums.OrderStatus;
@@ -77,7 +78,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 	
 	
